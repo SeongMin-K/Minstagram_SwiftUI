496d1bdbd26c0b7f70c7139ce12bb623c7196d40
SeongMin-K <eo1tks3@naver.com>
2021-11-19 17:47:57 +0900

    Initial Commit

---
+++ Minstagram_SwiftUI/Minstagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minstagram_SwiftUIApp.swift
+//  Minstagram_SwiftUI
+//
+//  Created by SeongMinK on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct Minstagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
